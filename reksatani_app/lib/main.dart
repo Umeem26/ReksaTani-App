701c6e495664a1348a136823c3fccf6c753886d4
Ihsan Ramadhan <ihsanhandsome06@gmail.com>
2026-04-23 19:36:56 +0700

    feat: inisialisasi koneksi mongodb atlas

--- reksatani_app/lib/main.dart
+++ reksatani_app/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'services/mongodb_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await MongoDatabase.connect();
   runApp(const MyApp());
 }
 
